d50d792f31c7dd2d8ef127e53f5075080326f414
Parves Hossain Rabby <parves.pe.wb.h@gmail.com>
2024-01-08 12:52:53 +0600

    oldie is goldie

--- src/utils/localDbRequests.js
+++ src/utils/localDbRequests.js
@@ -1,11 +1,62 @@
+// import { photos, videos } from "./db";
+// import { imageToBlob, videoToBlob } from "./mediaHandler";
+
+// export const addPhoto = async (photoData) => {
+//   try {
+//     const { userImageURL, largeImageURL, ...photo } = photoData;
+//     const userImageBlob = await imageToBlob(userImageURL);
+//     const largeImageBlob = await imageToBlob(largeImageURL);
+
+//     await photos.add({
+//       ...photo,
+//       userImageBlob,
+//       largeImageBlob,
+//     });
+
+//     return {
+//       status: "success",
+//       message: "Photo saved to offline storage",
+//     };
+//   } catch (error) {
+//     console.log(error);
+//     throw new Error("Something went wrong");
+//   }
+// };
+
+// export const addVideo = async (videoData) => {
+//   try {
+//     const { userImageURL, thumbImageURL, videoURL, ...video } = videoData;
+//     const userImageBlob = await imageToBlob(userImageURL);
+//     const thumbImageBlob = await imageToBlob(thumbImageURL);
+//     const videoBlob = await videoToBlob(videoURL, "video");
+
+//     return null;
+
+//     await videos.add({
+//       ...video,
+//       userImageBlob,
+//       thumbImageBlob,
+//       videoBlob,
+//     });
+
+//     return {
+//       status: "success",
+//       message: "Video saved to offline storage",
+//     };
+//   } catch (error) {
+//     console.log(error);
+//     throw new Error("Something went wrong");
+//   }
+// };
+
 import { photos, videos } from "./db";
-import { imageToBlob, videoToBlob } from "./mediaHandler";
+import { fileToBlob } from "./mediaHandler";
 
 export const addPhoto = async (photoData) => {
   try {
     const { userImageURL, largeImageURL, ...photo } = photoData;
-    const userImageBlob = await imageToBlob(userImageURL);
-    const largeImageBlob = await imageToBlob(largeImageURL);
+    const userImageBlob = await fileToBlob(userImageURL);
+    const largeImageBlob = await fileToBlob(largeImageURL);
 
     await photos.add({
       ...photo,
@@ -25,12 +76,12 @@ export const addPhoto = async (photoData) => {
 
 export const addVideo = async (videoData) => {
   try {
-    const { userImageURL, thumbImageURL, videoURL, ...video } = videoData;
-    const userImageBlob = await imageToBlob(userImageURL);
-    const thumbImageBlob = await imageToBlob(thumbImageURL);
-    const videoBlob = await videoToBlob(videoURL, "video");
+    console.log(videoData);
 
-    return null;
+    const { userImageURL, thumbImageURL, videoURL, ...video } = videoData;
+    const userImageBlob = await fileToBlob(userImageURL);
+    const thumbImageBlob = await fileToBlob(thumbImageURL);
+    const videoBlob = await fileToBlob(videoURL);
 
     await videos.add({
       ...video,
--- src/utils/mediaHandler.js
+++ src/utils/mediaHandler.js
@@ -1,9 +1,65 @@
-export const imageToBlob = async (url) => {
+// export const imageToBlob = async (url) => {
+//   try {
+//     const response = await fetch(url);
+
+//     if (!response.ok) {
+//       throw new Error(`Failed to fetch photo: ${response.statusText}`);
+//     }
+
+//     const arrayBuffer = await response.arrayBuffer();
+
+//     const contentType =
+//       response.headers.get("Content-Type") || "application/octet-stream";
+
+//     const blob = new Blob([arrayBuffer], {
+//       type: contentType,
+//     });
+
+//     return blob;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// export const videoToBlob = async (url) => {
+//   try {
+//     const response = await fetch(`${url}&download=1`, {
+//       method: "GET",
+//       headers: {
+//         "Allow-Access-Control-Origin": "*",
+//         "Content-Type": "video/mp4",
+//       },
+//     });
+
+//     console.log(response);
+
+//     if (!response.ok) {
+//       throw new Error(`Failed to fetch video: ${response.statusText}`);
+//     }
+
+//     const arrayBuffer = await response.arrayBuffer();
+
+//     const contentType =
+//       response.headers.get("Content-Type") || "application/octet-stream";
+
+//     const blob = new Blob([arrayBuffer], {
+//       type: contentType,
+//     });
+
+//     console.log(arrayBuffer, blob);
+
+//     return blob;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+export const fileToBlob = async (url) => {
   try {
     const response = await fetch(url);
 
     if (!response.ok) {
-      throw new Error(`Failed to fetch photo: ${response.statusText}`);
+      throw new Error(`Failed to fetch file: ${response.statusText}`);
     }
 
     const arrayBuffer = await response.arrayBuffer();
@@ -14,39 +70,6 @@ export const imageToBlob = async (url) => {
     const blob = new Blob([arrayBuffer], {
       type: contentType,
     });
-
-    return blob;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const videoToBlob = async (url) => {
-  try {
-    const response = await fetch(`${url}&download=1`, {
-      method: "GET",
-      headers: {
-        "Allow-Access-Control-Origin": "*",
-        "Content-Type": "video/mp4",
-      },
-    });
-
-    console.log(response);
-
-    if (!response.ok) {
-      throw new Error(`Failed to fetch video: ${response.statusText}`);
-    }
-
-    const arrayBuffer = await response.arrayBuffer();
-
-    const contentType =
-      response.headers.get("Content-Type") || "application/octet-stream";
-
-    const blob = new Blob([arrayBuffer], {
-      type: contentType,
-    });
-
-    console.log(arrayBuffer, blob);
 
     return blob;
   } catch (error) {
